709963302e4265d1e89ea6292d03a1705b474c30
Yonas Nieder Fernández <136090651+SortyFix@users.noreply.github.com>
2026-09-24 13:14:23 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false; 
+
+        HashMap<Character, Integer> hm1 = new HashMap<>();
+        HashMap<Character, Integer> hm2 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(hm1.containsKey(c)) {
+                int oldValue = hm1.get(c);
+                hm1.put(c, oldValue + 1);
+                continue;
+            }
+            hm1.put(c, 1);
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if(hm2.containsKey(c)) {
+                int oldValue = hm2.get(c);
+                hm2.put(c, oldValue + 1);
+                continue;
+            }
+            hm2.put(c, 1);
+        }
+
+        System.out.println(hm1.toString());
+        System.out.println(hm2.toString());
+
+        return Objects.equals(hm1, hm2);
+    }
+}
